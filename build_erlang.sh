b3583b14919d23820381ac92b0e2445c8c99e908
ShawnMa <fly2wind@gmail.com>
2013-01-05 07:53:02 -0800

    Create build_erlang.sh

---
+++ build_erlang.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+########################################################################################
+#
+# Install JDK Script
+# 
+# Shawn Ma
+#
+########################################################################################
+# Define
+TARGET=OTP_R15B03-1.tar.gz
+SOURCE=https://nodeload.github.com/erlang/otp/tar.gz/OTP_R15B03-1
+
+# Create a build directory
+mkdir -p /opt/install/erlang && cd /opt/install/erlang
+
+# Prepare for compilation source
+curl -o $TARGET $SOURCE
+mkdir -p tmp && tar -zxvf $TARGET -C tmp
+
+# Install build dependencies
+yum install -y ncurses-devel openssl-devel
+
+# Create a User Group
+
+# Compile and deploy
+mv tmp/* source && cd source
+./otp_build autoconf
+./configure \
+--prefix=/opt/environment/erlang/R15B03 \
+--enable-threads \
+--enable-smp-support \
+--enable-kernel-poll \
+--enable-hipe \
+--without-termcap \
+--without-javac \
+--with-ssl
+make
+make install
+
+# Postinstallation setup
+export PATH=$PATH:/opt/environment/erlang/R15B03/bin
+
+# Configuration
+
+# Additional
+sed -i '/export PATH=/a\export PATH=$PATH:\/opt\/environment\/erlang\/R15B03\/bin' /etc/profile
+
