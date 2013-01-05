b30b06a28617b66753decc4d55d245f31846708e
ShawnMa <fly2wind@gmail.com>
2013-01-05 07:52:20 -0800

    Create build_java.sh

---
+++ build_java.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+########################################################################################
+#
+# Install JDK Script
+# 
+# Shawn Ma
+#
+########################################################################################
+# Define
+TARGET=jdk-7u10-linux-x64.tar.gz
+SOURCE=http://www.ganshazi.com/jdk-7u10-linux-x64.tar.gz
+
+# Create a build directory
+mkdir -p /opt/install/java && cd /opt/install/java
+
+# Prepare for compilation source
+curl -o $TARGET $SOURCE
+mkdir -p tmp && tar -zxvf $TARGET -C tmp
+
+# Install build dependencies
+
+# Create a User Group
+
+# Compile and deploy
+mkdir -p /opt/environment/java && mv tmp/* /opt/environment/java/jdk1.7
+
+# Postinstallation setup
+export PATH=$PATH:/opt/environment/java/1.7/bin
+
+# Configuration
+sed -i '/export PATH$/a\export PATH=$PATH:\/opt\/environment\/java\/1.7\/bin' /etc/profile
+
+# Additional
+
+
+
