660c29d5f2ed8837eb51477abe9dbd3db05375b9
ShawnMa <fly2wind@gmail.com>
2013-07-02 15:18:55 +0800

    Update build_tomcat.sh

--- build_tomcat.sh
+++ build_tomcat.sh
@@ -7,8 +7,8 @@
 #
 ########################################################################################
 # Define
-TARGET=apache-tomcat-7.0.39.tar.gz
-SOURCE=http://apache.etoak.com/tomcat/tomcat-7/v7.0.39/bin/$TARGET
+TARGET=apache-tomcat-7.0.41.tar.gz
+SOURCE=http://mirror.bit.edu.cn/apache/tomcat/tomcat-7/v7.0.41/bin/$TARGET
 
 # Create a build directory
 mkdir -p /opt/install/tomcat && cd /opt/install/tomcat
